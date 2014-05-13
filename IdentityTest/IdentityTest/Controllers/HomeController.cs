025e0904f4d02b544a42c9a994195756045be12f
EliasCole3 <eliascole3@gmail.com>
2014-05-13 14:05:16 -0700

    Initial commit

---
+++ IdentityTest/IdentityTest/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IdentityTest.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            string temp = user.Password;
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+
+            var userIdentity = new IdentityUser() { UserName = user.Username };
+            IdentityResult result = manager.Create(userIdentity, user.Password);
+
+            if (result.Succeeded)
+            {
+                ViewBag.Message = "Identity user create worked";
+            }
+            return View("Index");
+        }
+
+    }
+}
